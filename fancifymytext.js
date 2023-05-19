1d8cb59c2a9263034b24899f1d82b657c4403a80
framos381 <framos381@gmail.com>
2023-05-18 19:00:24 -0700

    more changes

--- fancifymytext.js
+++ fancifymytext.js
@@ -4,14 +4,14 @@ function makeBigger(){
     textbox.style.fontSize="24pt";
 }
 
-document.getElementById("fancy").addEventListener("change",function() {
+document.getElementByName("fancification")[0].addEventListener("change",function() {
     textbox.style.color = "blue";
     textbox.style.fontWeight="bold";
     textbox.style.textDecoration="underline";
     alert("Look at the style!");
 })
 
-document.getElementById("boring").addEventListener("change",function() {
+document.getElementByName("fancification")[1].addEventListener("change",function() {
     textbox.style.color="black";
     textbox.style.fontweight="normal";
     textbox.style.textDecoration="none";
@@ -19,7 +19,7 @@ document.getElementById("boring").addEventListener("change",function() {
 })
 
 function makeMoo(){
-    textbox.style.textTransform="uppercase";
+    textbox.style.textTransform = "uppercase";
     let text1 = textbox.value.split(".");
     let text2 = text1.join("-Moo");
     textbox.value = text2;
